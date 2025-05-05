07865db3a5ef1f207e57e8507433ddf0c0eef54d
1mGoodDev <sulthon.elmanaar@gmail.com>
2025-05-05 21:00:24 +0700

    add adminInfos feature

--- app/Http/Controllers/api/NotificationController.php
+++ app/Http/Controllers/api/NotificationController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ApiResource;
+use App\Models\AdminInfo;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    //
+    public function getAdminInfo() {
+        $adminInfo = AdminInfo::latest();
+        return new  ApiResource(true, 'Data berhasil diambil', $adminInfo);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\CommentController;
 use App\Http\Controllers\api\FollowController;
 use App\Http\Controllers\api\LikeController;
+use App\Http\Controllers\api\NotificationController;
 use App\Http\Controllers\api\PostController;
 use App\Http\Controllers\api\ProfileController;
 use Illuminate\Http\Request;
@@ -19,6 +20,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('feed', PostController::class);
     Route::get('feed-by-following', [PostController::class, 'following']);
+    Route::get('admin-info', [NotificationController::class, 'getAdminInfo']);
 
     Route::post('post/{id}/like', [LikeController::class, 'like']);
     Route::post('post/{id}/comment', [CommentController::class, 'storeComment']);
